c8d01974609973c7cf71a423700e3fe1819757a5
Krzysztof Zwolenik <kzwolenik95@gmail.com>
2025-03-08 16:09:55 +0100

    Add missing quotes, fix typo

--- ansible/roles/mongo-backup-service/files/backup-to-digitalocean-bucket.sh
+++ ansible/roles/mongo-backup-service/files/backup-to-digitalocean-bucket.sh
@@ -4,7 +4,7 @@ filename="dump-$(date '+%Y%m%d%H%M%S').gz"
 
 mongodump --gzip --archive="${filename}"
 
-export AWS_ACCESS_KEY_ID=$(jq -r '.id' < "${CREDENTIALS_DIRECTORY}/do_access_secret)"
+export AWS_ACCESS_KEY_ID=$(jq -r '.id' < "${CREDENTIALS_DIRECTORY}/do_access_secret")
 export AWS_SECRET_ACCESS_KEY=$(jq -r '.key' < "${CREDENTIALS_DIRECTORY}/do_access_secret")
 
 s3cmd --host="fra1.digitaloceanspaces.com" \
